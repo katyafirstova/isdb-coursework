15f2a2cef2c89d1527656bf496024bb8603bc765
katyafirstova <firstovakatya42@gmail.com>
2023-03-22 21:36:35 +0300

    update point

--- src/main/java/application/controller/PointController.java
+++ src/main/java/application/controller/PointController.java
@@ -1,29 +1,29 @@
-//package application.controller;
-//
-//import application.model.Point;
-//import application.repository.PointRepository;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.List;
-//
-//
-//@RestController
-//@RequestMapping("api")
-//public class PointController {
-//
-//
-//   private final PointRepository pointRepository;
-//
-//    public PointController(PointRepository pointRepository) {
-//        this.pointRepository = pointRepository;
-//    }
-//
-//    @GetMapping("/points")
-//    public List<Point> getAllPoints() {
-//        return pointRepository.findAll();
-//    }
-//
-//
-//}
+package application.controller;
+
+import application.model.Point;
+import application.repository.PointRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("api")
+public class PointController {
+
+
+   private final PointRepository pointRepository;
+
+    public PointController(PointRepository pointRepository) {
+        this.pointRepository = pointRepository;
+    }
+
+    @GetMapping("/points")
+    public List<Point> getAllPoints() {
+        return pointRepository.findAll();
+    }
+
+
+}
--- src/main/java/application/repository/PointRepository.java
+++ src/main/java/application/repository/PointRepository.java
@@ -1,13 +1,14 @@
-//package application.repository;
-//
-//import application.model.Point;
-//import application.model.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.List;
-//
-//@Repository
-//public interface PointRepository extends JpaRepository<Point, Long> {
-//    List<Point> findByUser(User user);
-//}
+package application.repository;
+
+import application.model.Point;
+import application.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PointRepository extends JpaRepository<Point, Long> {
+    List<Point> findByUserName(String userName);
+    void deleteByUserName(String userName);
+}
