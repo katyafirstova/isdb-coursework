e4a1be622e2b5c58239a569417813b376436db78
katyafirstova <firstovakatya42@gmail.com>
2023-03-22 21:37:18 +0300

    update point

--- src/main/java/exceptions/UserNotFoundException.java
+++ src/main/java/exceptions/UserNotFoundException.java
@@ -2,6 +2,6 @@ package exceptions;
 
 public class UserNotFoundException extends RuntimeException{
     public UserNotFoundException(Long id) {
-        super("Could not find employee " + id);
+        super("Could not find user " + id);
     }
 }
