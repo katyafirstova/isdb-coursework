a4a6a1af3404f15ff7e60cf7824e998268c09c8d
katyafirstova <firstovakatya42@gmail.com>
2023-03-23 21:41:06 +0300

    front

--- src/main/java/org/itmo/web4/controller/AuthController.java
+++ src/main/java/org/itmo/web4/controller/AuthController.java
@@ -1,64 +1,68 @@
-package org.itmo.web4.controller;//package controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.web.bind.annotation.*;
-//import spring.jwt.JwtUtils;
-//import spring.model.User;
-//import auth.JwtResponse;
-//import auth.LoginRequest;
-//import auth.ResponseMessage;
-//import auth.SignupRequest;
-//import spring.repository.UserRepository;
-//import spring.services.UserDetailsImpl;
-//
-//@RestController
-//@RequestMapping("/api/auth")
-//@CrossOrigin(origins = "*", maxAge = 3600)
-//public class AuthController {
-//
-//    @Autowired
-//    AuthenticationManager authenticationManager;
-//    @Autowired
-//    UserRepository userRepository;
-//    @Autowired
-//    PasswordEncoder passwordEncoder;
-//    @Autowired
-//    JwtUtils jwtUtils;
-//
-//    UserDetailsImpl userDetails;
-//
-//    @PostMapping("/signin")
-//    public ResponseEntity<?> authUser(@RequestBody LoginRequest loginRequest) {
-//        Authentication authentication = authenticationManager.authenticate
-//                (new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
-//
-//        SecurityContextHolder.getContext().setAuthentication(authentication);
-//        String jwt = jwtUtils.generateJwtToken(authentication);
-//
-//
-//        return ResponseEntity.ok(new JwtResponse(jwt, userDetails.getId(), userDetails.getUsername()));
-//
-//    }
-//
-//    public ResponseEntity<?> registerUser(@RequestBody SignupRequest signupRequest) {
-//        if (userRepository.existsByUsername(signupRequest.getUsername())) {
-//            return ResponseEntity.badRequest().body(new ResponseMessage("This username is already exists"));
-//        }
-//
-//        User user = new User(signupRequest.getUsername(), passwordEncoder.encode(signupRequest.getPassword()));
-//        userRepository.save(user);
-//
-//        return ResponseEntity.ok(new ResponseMessage("User created"));
-//
-//
-//    }
-//
-//
-//}
-//
+package org.itmo.web4.controller;
+
+import org.itmo.web4.auth.JwtResponse;
+import org.itmo.web4.auth.LoginRequest;
+import org.itmo.web4.auth.ResponseMessage;
+import org.itmo.web4.auth.SignupRequest;
+import org.itmo.web4.jwt.JwtUtils;
+import org.itmo.web4.model.User;
+import org.itmo.web4.repository.UserRepository;
+import org.itmo.web4.services.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+
+@CrossOrigin
+public class AuthController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    JwtUtils jwtUtils;
+
+    UserDetailsImpl userDetails;
+
+    @PostMapping("/auth")
+    public ResponseEntity<?> authUser(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate
+                (new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+
+        return ResponseEntity.ok(new JwtResponse(jwt, userDetails.getId(), userDetails.getUsername()));
+
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ResponseEntity<?> registerUser(@RequestBody SignupRequest signupRequest) {
+        if (userRepository.existsByUsername(signupRequest.getUsername())) {
+            return ResponseEntity.badRequest().body(new ResponseMessage("This username is already exists"));
+        }
+
+        User user = new User(signupRequest.getUsername(), passwordEncoder.encode(signupRequest.getPassword()));
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new ResponseMessage("User created"));
+
+
+    }
+
+
+}
+
