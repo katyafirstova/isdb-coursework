4dc11a00451193727fc8990acf677cccd9c97e98
Firstova Ekaterina <70884895+katyafirstova@users.noreply.github.com>
2024-03-02 23:48:40 +0300

    Delete frontend directory

--- frontend/src/app/model/auth-response.ts
+++
@@ -1,4 +0,0 @@
-export class AuthResponse{
-    message = '';
-
-}
--- frontend/src/app/model/point-response.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-import {Point} from "./point.component";
-
-
-export interface PointResponse {
-  data: Point[]
-
-}
